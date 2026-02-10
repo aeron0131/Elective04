b27bbb456cddd5d2f79d20bcf79b1a4361881133
joemar <joemarverueco78@gmail.com>
2026-02-10 20:07:11 +0800

    Modify test_acne_detector file

--- tests/test_acne_detector.py
+++ tests/test_acne_detector.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(_file_), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from image_processing import acne_detector  
 
